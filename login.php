26d23e45bda7bce5bdf47d268f0df59f9a64bee8
ZPaulo <jose.p.s.olive@gmail.com>
2014-11-27 03:12:01 +0000

    login working
    Signed-off-by: ZPaulo <jose.p.s.olive@gmail.com>

--- login.php
+++ login.php
@@ -1,55 +1,39 @@
 <?php
 
-
 function check_user($user,$hashPass){
-    $db = new PDO('sqlite:db/dataBase.db');
-    $chk = $db->prepare('SELECT * FROM User WHERE user = ?');
-    $chk->execute(array($user));
-	$row = $chk->fetch();
-    if(!$row)
-	{	
-		if($row['password'] == $hashPass)
-			return true;
-	}
-      return false;
+  $db = new PDO('sqlite:db/dataBase.db');
+
+  $stmt = $db->prepare('SELECT * FROM User WHERE user = ? AND password = ?');
+  $stmt->execute(array($user, $hashPass));
+
+  return $stmt->fetch() !== false;
 }
 
 
 function login(){
-try {
-		$user = $_POST['user'];
-		$hashPass=md5($_POST['pass']);
-		$pass = $hashPass;
-		if(check_user($user,$hashPass))
-		{
-			$msg = "Succecefull Login";
-			echo $msg;
-			return true;
-		}
-		else
-		{
-			$msg = "Invalid UserName or Password";
-			echo $msg;
-			return false;
-		}
+  try {
+    $user = $_POST['user'];
+    $hashPass=md5($_POST['pass']);
+    if(check_user($user,$hashPass))
+    {
+      $msg = "Succecefull Login";
+      return $msg;
+    }
+    else
+    {
+      $msg = "Invalid UserName or Password";
+      return $msg;
+    }
 
-		
-	} catch (PDOException $e) {
+  } catch (PDOException $e) {
     echo $e->getMessage();
   }
 }
-$value=login();
-echo $value;
-if($value)
-{
-header('Location: register_body.php');
-}
-else
-{
-echo $value;
-	//header('Location: fake.php');
-}
 
-?>	
-	
-	
+session_start();
+
+$_SESSION['Msg'] = login();
+$_SESSION['username'] = $_POST['user'];
+
+header('Location: ' . $_SERVER['HTTP_REFERER']);
+?>
