a567fd7b6cc48b42b88245304e1225389e66db68
joaoBordalo <bordalo.joao94@gmail.com>
2014-12-01 10:21:48 +0000

    verificacoes feitas no register

--- register.php
+++ register.php
@@ -11,26 +11,76 @@ $db = new PDO('sqlite:db/dataBase.db');
       return false;
   }
 
+  function check_name($name){
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) 
+      return false;
+    else
+      return true;
+  }
+
+  function check_user_name($user){
+
+    if(!preg_match("/^[a-zA-Z0-9]*$/",$user))
+      return false;
+    else
+      return true;
+
+  }
+
+  function check_email_exists($email){
+    global $db;
+    $chk = $db->prepare('SELECT * FROM User WHERE email = ?');
+    $chk->execute(array($email));
+    if(!$chk->fetch())
+      return true;
+    else
+      return false;
+  }
+
+
 function register_user(){
   global $db;
   try {
     $user = $_POST['user'];
     $name = $_POST['name'];
     $email = $_POST['email'];
-	  $hashPass=md5($_POST['pass']);
-    $pass = $hashPass;
-    if(check_user($user))
-    {
-      $ins = $db->prepare('INSERT INTO User (user,name,email,password) Values (?, ?, ?, ?)');
-      $ins->execute(array($user,$name,$email,$pass));
-      $msg = "Registration was succesfull :)";
-      return $msg;
-    }
-    else
-      {
-        $msg = "Username already taken, please choose another";
-        return $msg;
-      }
+	  $hashPass=$_POST['pass'];
+    $confirmationPass = $_POST['confPass'];
+
+
+  if($confirmationPass!=$hashPass)
+  {
+    $msg = "Passwords don't match";
+    return $msg;
+  }
+  else if(!check_user_name($user))
+  {
+    $msg = "Only letters and numbers allowed for UserName";
+    return $msg;
+  }
+  else if(!check_name($name))
+  {
+    $msg = "Only letters and white space allowed for Name";
+    return $msg;
+  }
+  else if(!check_email_exists($email))
+  {
+    $msg = "E-mail already used";
+    return $msg;
+  }
+  else if(!check_user($user))
+  {
+    $msg = "Username already taken, please choose another";
+    return $msg;
+  }
+  else
+  {
+    $pass = md5($hashPass);
+    $ins = $db->prepare('INSERT INTO User (user,name,email,password) Values (?, ?, ?, ?)');
+    $ins->execute(array($user,$name,$email,$pass));
+    $msg = "Registration was succesfull :)";
+    return $msg;
+  }
 
   } catch (PDOException $e) {
     echo $e->getMessage();
--- register_body.php
+++ register_body.php
@@ -2,10 +2,11 @@
 
 <link rel="stylesheet" href="style.css" type="text/css">
   <form id="Register" action = "register.php" method = "post">
-    <div class="Info">UserName:<input type="text" name = "user">
-                      Name:<input type="text" name = "name">
-                      Email:<input type="text" name = "email">
-                      Password:<input type="password" name = "pass">
+    <div class="Info">UserName:<input type="text" name = "user" required="true">
+                      Name:<input type="text" name = "name" required="true">
+                      Email:<input type="email" name = "email" required="true">
+                      Password:<input type="password" name = "pass" required="true">
+                      Confirm Password:<input type="password" name="confPass" required="true">
     </div>
     <input type="submit" value="Register"/>
 </form>
