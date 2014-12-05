11c1e023876c9a7a4ee91312dca7e49443a330f8
joaoBordalo <bordalo.joao94@gmail.com>
2014-12-05 05:53:32 +0000

    manage done ( ao menos faço commits de jeito)

---
+++ delete.php
@@ -0,0 +1,30 @@
+<?php
+
+$db = new PDO('sqlite:db/dataBase.db');
+
+function delete($idPoll)
+{
+
+  global $db;
+
+    $updP = $db->prepare('DELETE FROM Poll WHERE idPoll=?');
+    $updP->execute(array($idPoll));
+
+	$updQ = $db->prepare('DELETE FROM Question WHERE idPoll=?');
+    $updQ->execute(array($idPoll));
+
+    $updA = $db->prepare('DELETE FROM Answer WHERE idPoll=?');
+    $updA->execute(array($idPoll));
+
+    $updA = $db->prepare('DELETE FROM UserAnswers WHERE idPoll=?');
+    $updA->execute(array($idPoll));
+
+
+
+}
+
+delete($_GET['idPoll']);
+
+header('Location: profile_body.php');
+
+?>
--- profile_body.php
+++ profile_body.php
@@ -1,5 +1,6 @@
 <?php include_once('templates/header.php'); ?>
 
+
         <div id = "profile">
           <?php include_once('profile.php'); ?>
         </div>
