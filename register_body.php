d78a4c420b4a951d3ed0d79938d77247922253fe
Casquipster <joaosoaresacs@gmail.com>
2014-12-05 01:11:28 +0000

    o ze é o pior webdesigner de sempre

--- register_body.php
+++ register_body.php
@@ -1,57 +1,63 @@
 
 	<?php include_once('templates/header.php'); ?>
+<script type="text/javascript">
+ $(window).load(function(){
+  $('#registerModal').modal('show');
+ });
+ </script>
 
-	<div class="modal fade" id="myModal" tabindex="1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
+
+	<div class="modal fade" id="registerModal" tabindex="1" role="dialog" aria-labelledby="registerModalLabel" aria-hidden="false">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+        <h1 class="modal-title" id="registerModalLabel">REGISTER</h1>
       </div>
       <div class="modal-body">
 <div class="jumbotron" id="j1">
 	<div class = "container">
 		<div id="register">
-			<h1><b>REGISTER</b></h1><br>
 			<form class="form-horizontal" role="form" action="register.php" method="post">
 				<div class="form-group">
-					<label for="user" class="col-sm-2 control-label">USERNAME</label>
+					<!--<label for="user" class="col-sm-2 control-label">USERNAME</label>-->
 					<div class="col-sm-10">
 						<input type="text" name="user" class="form-control" id="user"
 						required="true" placeholder="USERNAME">
 					</div>
-					<label for="name" class="col-sm-2 control-label">NAME</label>
+					
 					<div class="col-sm-10">
 						<input type="text" name="name"  class="form-control" id="name"
 						required="true" placeholder="NAME">
 					</div>
-					<label for="email" class="col-sm-2 control-label">E-MAIL</label>
+					
 					<div class="col-sm-10">
 						<input type="email" name="email"  class="form-control" id="email"
 						required="true" placeholder="E-MAIL">
 					</div>
-					<label for="password" class="col-sm-2 control-label">PASSWORD</label>
+					
 					<div class="col-sm-10">
 						<input type="password" name="pass" class="form-control" id="input" required="true" placeholder="PASSWORD">
 					</div>
 					<span id="output"></span>
-					<label for="password" class="col-sm-2 control-label">CONFIRM PASSWORD</label>
+					
 					<div class="col-sm-10">
 						<input type="password"  name="confPass" class="form-control" id="confPass" required="true" placeholder="CONFIRM PASSWORD">
 					</div>
 					  <script type="text/javascript" src="js/strong_password.js"></script>
 					<div class="col-sm-10">
-						 <button id ="button" type="submit"> Register </button>
+						<button type="submit" id="button" class="btn btn-primary">REGISTER</button>
 					</div>
 				</div>
-			</form>
+			 </form>
 		</div>
 	</div>
 </div>
 </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+       
+       
       </div>
     </div>
   </div>
--- vote_poll_body.php
+++ vote_poll_body.php
@@ -1,29 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400,600,700,800' rel='stylesheet' type='text/css'>
-
-  <title>YOUPOLL</title>
-  <link rel="stylesheet" href="templates/menustyle.css" type="text/css">
-  <link rel="stylesheet" href="templates/style.css" type="text/css">
-  <meta charset = "UTF-8">
-</head>
-<body>
-  <div id="header">
-    <div id="cssmenu">
-      <ul>
-        <li><a href='index.php'><span>HOMEPAGE</span></a></li>
-        <?php session_start();
-        if(isset($_SESSION['username'])){ ?>
-
-          <li><a href='logout.php'><span>Logout</span></a></li>
-          <?php } ?>
-          <!--<li><a href=""><span>SEARCH</span></a></li>
-          <li class='last'><a href=""><span>PROFILE</span></a></li>-->
-        </ul>
-      </div>
-    </div>
-
+<?php include_once('templates/header.php'); ?>
 <?php
   session_start();
 
@@ -32,14 +7,14 @@
   <?php
   unset($_SESSION['Msg']);
   ?>
-<div id="container">
+
 <form id="Vote" action = <?php echo "vote.php?poll=". $_GET['poll'] ?> method = "post">
-  <div class = "Poll">
-    <fieldset>
-      <?php require('load_vote.php');?>
+  <div id="voteContainer">
+    
+    <?php require('load_vote.php');?>
   </div>
 </form>
-</div>
+
 
 
 
