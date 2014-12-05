0f02d4dfbece50d7ffc0d612672e031a0fb133b1
ZPaulo <jose.p.s.olive@gmail.com>
2014-12-05 00:23:25 +0000

    share
    Signed-off-by: ZPaulo <jose.p.s.olive@gmail.com>

--- profile_body.php
+++ profile_body.php
@@ -7,8 +7,9 @@
   <link rel="stylesheet" href="templates/menustyle.css" type="text/css">
   <link rel="stylesheet" href="templates/style.css" type="text/css">
   <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-  <link type="text/css" rel="stylesheet" href="templates/menupopup.css" />
   <script type="text/javascript" src="js/jquery.leanModal.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" />
   <meta charset = "UTF-8">
 </head>
@@ -27,18 +28,8 @@
       </div>
     </div>
 
-    <?php
-    session_start();
-
-    if(isset($_SESSION['Msg']))
-    ?> <div id = "errorMsg"> <?php echo $_SESSION['Msg'];?> </div>
-    <?php
-    unset($_SESSION['Msg']);
-    ?>
         <div id = "profile">
-          <ul>
-            <?php require('profile.php');?>
-          </ul>
-
+          <?php include_once('profile.php'); ?>
+        </div>
 
       <?php include_once('templates/footer.php'); ?>
--- share_popup.php
+++ share_popup.php
@@ -1,47 +1,58 @@
-<input type="button" id="modal_trigger" href="#modal"  value="Share">
+<?php
+$rep = strrchr($_SERVER['REQUEST_URI'],'/');
+$uri =$_SERVER['REQUEST_URI'];
+$path = str_replace($rep,"/vote_poll_body.php?poll=".$row['name'],$uri);
+$url = $_SERVER['SERVER_NAME'].$path;
+?>
 
- <div id="modal" class="popupContainer" style="display:none;">
-    <header class="popupHeader">
-      <span class="header_title">Share with Friends</span>
-      <span class="modal_close"><i class="fa fa-times"></i></span>
-    </header>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#shareModal" data-poll=<?php echo $row['name']; ?> data-url=<?php echo $url; ?> >Share</button>
 
-    <section class="popupBody">
-      <!-- Social  -->
-      <div class="social_login">
-        <div class="">
-            <input type="url" name = "url" value=<?php  $rep = strrchr($_SERVER['REQUEST_URI'],'/'); $uri =$_SERVER['REQUEST_URI'];  $path = str_replace($rep,"/vote_poll_body.php?poll=".$row['name'],$uri); echo $url = $_SERVER['SERVER_NAME'].$path; ?> >
-
-            <a href=<?php echo "https://www.facebook.com/sharer/sharer.php?u=".$url; ?> target="_blank" class="social_box fb">
-            <span class="icon"><i class="fa fa-facebook"></i></span>
-            <span class="icon_title">Share on Facebook</span>
-          </a>
-
-          <a href="#" class="social_box google">
-            <span class="icon"><i class="fa fa-google-plus"></i></span>
-            <span class="icon_title">Share on Google</span>
-          </a>
-
-          <a href="#"id="mail" class="social_box mail">
-            <span class="icon"><i class="fa fa-archive"></i></span>
-            <span class="icon_title">Share by e-mail</span>
-          </a>
-        </div>
+<div class="modal fade" id="shareModal" tabindex="-1" role="dialog" aria-labelledby="shareModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="shareModalLabel">Share with anyone</h4>
       </div>
-    </section>
+      <div class="modal-body">
+        <form role="form">
+          <div class="form-group">
+            <label for="shareLink-name" class="control-label">Link:</label>
+            <input type="url" name = "url" value=<?php  echo $url; ?> >
+          </div>
+          <div class="form-social">
+            <a href="" target="_blank" class="social_box_fb">
+              <span class="icon"><i class="fa fa-facebook"></i></span>
+              <span class="icon_title">Share on Facebook</span>
+            </a>
+
+            <a href="#" class="social_box google">
+              <span class="icon"><i class="fa fa-google-plus"></i></span>
+              <span class="icon_title">Share on Google</span>
+            </a>
+
+            <a href="#"id="mail" class="social_box mail">
+              <span class="icon"><i class="fa fa-archive"></i></span>
+              <span class="icon_title">Share by e-mail</span>
+            </a>
+          </div>
+      </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
   </div>
+</div>
 
-<script type="text/javascript">
-
-  $("#modal_trigger").leanModal({top : 200, overlay : 0.6, closeButton: ".modal_close" });
-
-
-  $(function(){
-    // Calling Login Form
-    $("#mail").click(function(){
-      $(".social_login").hide();
-      return false;
-    });
-
-  })
+<script>
+$('#shareModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget)
+  var poll = button.data('poll')
+  var url = button.data('url')
+  var modal = $(this)
+  modal.find('.modal-body input').val(url)
+  modal.find('.modal-title').text("Share "+poll+"  with anyone!")
+  $('.social_box_fb').attr("href", "https://www.facebook.com/sharer/sharer.php?u="+url);
+})
 </script>
